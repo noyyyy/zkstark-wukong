d3ec575711bf76fbc7febcb5cb4a3ba7bfe2c85d
Noy <59097122+noyyyy@users.noreply.github.com>
2024-08-26 21:33:50 +0800

    feat: use plugin use copy

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,10 +3,23 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), wasm(), topLevelAwait()],
+    plugins: [
+        react(),
+        wasm(),
+        topLevelAwait(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, "../cocos/build/web-desktop/*"),
+                    dest: "cocos-build",
+                },
+            ],
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
